7d49b227078a52910ed2809aaf6fb3bf4b425a0f
TEmert22 <108573691+TEmert22@users.noreply.github.com>
2024-12-09 18:00:31 +0000

    Update __main__.py

--- __main__.py
+++ __main__.py
@@ -1,12 +1,12 @@
-from tkinter import *
+#import GUI
 import tkinter as tk
 #Import finance module
 import yfinance as yf
-#Import maths modules
+#Import graph processing modules
 import matplotlib.pyplot as plt
 from matplotlib.figure import Figure
-from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg, NavigationToolbar2Tk 
-import pandas as pd
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+import pyautogui
 #Import word processing modules
 import nltk
 from nltk import pos_tag, RegexpParser
@@ -17,7 +17,7 @@ class Stock:
     def __init__(self, ticker):
         self.ticker = ticker
         self.data = yf.Ticker(ticker)
-        self.price_data = self.data.history(period="1y", interval="1d")
+        self.price_data = self.data.history(period='1y', interval='1d')
         self.summary = self.data.info['longBusinessSummary']
         #Discovering name from description
         pos_tagged_summary = pos_tag(word_tokenize(self.summary))
@@ -34,44 +34,48 @@ class Stock:
 
     #Plot candle chart
     def candle_chart(self, tk_window):
+        #Reset canvas to display new graph
+        global canvas
+        try:
+            canvas.get_tk_widget().destroy()
+        except:
+            canvas.destroy()
+        #Define plot resolution
+        #Screen_size = (width, height)
+        screen_size = pyautogui.size()
+        dpi = 120
         #Initialise Plot and set outer plot to black
-        graph_figure = Figure(figsize=(10,10), dpi=80)
+        graph_figure = Figure(figsize=(screen_size[0]/dpi, screen_size[1]/dpi), dpi=dpi)
         graph = graph_figure.add_subplot(111)
-        graph_figure.set_facecolor('#000000')
+        graph_figure.set_facecolor(col_black)
         #Define graph appearance
         bullish = self.price_data[self.price_data.Close >= self.price_data.Open]
         bearish = self.price_data[self.price_data.Close < self.price_data.Open]
-        bullish_colour = "green"
-        bearish_colour = "red"
-        candle_width = 0.9
+        candle_width = 0.8
         wick_width = candle_width*0.15
         #Plot bullish periods on chart
         #candle - top wick - bottom wick
-        graph.bar(bullish.index, bullish.Close-bullish.Open, candle_width, bottom=bullish.Open, color=bullish_colour)
-        graph.bar(bullish.index, bullish.High-bullish.Close, wick_width, bottom=bullish.Close, color=bullish_colour)
-        graph.bar(bullish.index, bullish.Open-bullish.Low, wick_width, bottom=bullish.Low, color=bullish_colour)
+        graph.bar(bullish.index, bullish.Close-bullish.Open, candle_width, bottom=bullish.Open, color=col_green)
+        graph.bar(bullish.index, bullish.High-bullish.Close, wick_width, bottom=bullish.Close, color=col_green)
+        graph.bar(bullish.index, bullish.Open-bullish.Low, wick_width, bottom=bullish.Low, color=col_green)
         #Plot bearish periods on chart
         #candle - top wick - bottom wick
-        graph.bar(bearish.index, bearish.Open-bearish.Close, candle_width, bottom=bearish.Close, color=bearish_colour)
-        graph.bar(bearish.index, bearish.High-bearish.Open, wick_width, bottom=bearish.Open, color=bearish_colour)
-        graph.bar(bearish.index, bearish.Close-bearish.Low, wick_width, bottom=bearish.Low, color=bearish_colour)
-        graph.tick_params(axis='x', colors = '#FFFFFF', rotation = 30, labelsize = 12)
-        graph.tick_params(axis='y', colors = '#FFFFFF', labelsize = 15)        #BG colour set to dark blue - Title - Grid
-        graph.set_facecolor("#233D4D")
-        graph.set_title(f"{self.name} price history (1Y period)", color= '#FFFFFF')
-        graph.grid(True, color = "#9DA39A")
-        #Creating Tkinter Canvas which contains chart
+        graph.bar(bearish.index, bearish.Open-bearish.Close, candle_width, bottom=bearish.Close, color=col_red)
+        graph.bar(bearish.index, bearish.High-bearish.Open, wick_width, bottom=bearish.Open, color=col_red)
+        graph.bar(bearish.index, bearish.Close-bearish.Low, wick_width, bottom=bearish.Low, color=col_red)
+        graph.tick_params(axis='x', colors = col_white, rotation = 30, labelsize = 9)
+        graph.tick_params(axis='y', colors = col_white, labelsize = 9)
+        #BG colour set to dark blue - Title - Grid
+        graph.set_facecolor(col_darkblue)
+        graph.set_title(f'{self.name} price history (1Y period)', color= col_white)
+        graph.grid(True, color = col_grey, alpha=0.3)
         canvas = FigureCanvasTkAgg(graph_figure, master = tk_window)
         canvas.draw()
-        canvas.get_tk_widget().grid(row=2,column=0)
-        #canvas.get_tk_widget().pack(side = tk.BOTTOM, fill=tk.BOTH, expand=True)
-        #Creating matplotlib toolbar
-        toolbar = NavigationToolbar2Tk(canvas, tk_window)
-        toolbar.update()
-        canvas.get_tk_widget().pack()
+        canvas.get_tk_widget().pack(side = tk.BOTTOM, fill=tk.BOTH, expand=True)
+
 #Function checks whether ticker exists
 def tickerExists(ticker):
-    if ticker == "":
+    if ticker == '':
         return False
     else:
         ticker_test = yf.Ticker(ticker).history(period='5d', interval='1d')
@@ -79,49 +83,83 @@ def tickerExists(ticker):
             return True
         else:
             return False
-#Window processing here
-default_font = ('calibre',10,'bold')
-return_label = None
+        
+
 #Function to return failed results to the user
-def tickerHandling(ticker, tk_window):
-    global return_label
-    if return_label is not None:
-        return_label.destroy()
+def tickerHandling(*args):
+    ticker = stock_name.get()
+    if returnframe.winfo_viewable():
+        returnframe.grid_remove()
     if tickerExists(ticker):
         stock = Stock(ticker)
-        stock.candle_chart(tk_window)
-    else:   
-        if ticker == "":
-            return_text = "Please input a ticker before searching."
+        stock.candle_chart(graphframe)
+    else:
+        if ticker == '':
+            return_label.configure(text='Error: Please input a ticker before searching.')
         else:
-            return_text = f"Error: ticker {ticker.upper()} does not exist."
-    return_label= tk.Label(tk_window, text=return_text, font=default_font, fg="red")
-    return_label.grid(row=1,column=0)
+            return_label.configure(text=f'Error: ticker {ticker.upper()} is not available or does not exist.')
+        returnframe.grid(column=0, row=1, sticky='EW')
+        returnframe.grid_columnconfigure(0, weight=1)
+        delete_return_frame()
 
-#Initialise Stock object 
-#while True:
-#    ticker_name = input("Ticker Search: ").upper()
-#    if tickerExists(ticker_name):
-#        stock = Stock(ticker_name)
-#        break
-#stock = Stock('GOOG')
+#Function to stop scheduled deletion of error message frame, letting it be updated instead
+delete_frame_id = ''
+def delete_return_frame(*args):
+    global delete_frame_id
+    if delete_frame_id != '':
+        root.after_cancel(delete_frame_id)
+    delete_frame_id = root.after(5000, returnframe.grid_remove)           
+
+#WINDOW CREATION SECTION
+#Window Styling
+default_font = ('calibre', 10, 'bold')
+error_font = ('calibre', 15, 'bold')
+col_white = '#FFFFFF'
+col_grey = '#9DA39A'
+col_black = '#000000'
+col_darkblue = '#233D4D'
+col_red = '#FF495C'
+col_green = '#5AFF15'
 
 #Create the window
-window = Tk()
-window.title('Stock Graph in Tkinter')
-window.geometry('500x500')
-#Create search box and variable to store results
-stock_name = tk.StringVar()
-search_label = tk.Label(window, text = "Stock: ", font=default_font)
-search_box = tk.Entry(window, textvariable=stock_name, font=default_font)
-search_button = Button(master = window,
-                     command = lambda: tickerHandling(stock_name.get(), window),
-                     height = 2,
-                     width = 10,
-                     text = "Search")
-#Organising window elements using grid
-search_label.grid(row=0,column=0)
-search_box.grid(row=0,column=1)
-search_button.grid(row=0,column=2)
-window.mainloop()
+root = tk.Tk()
+root.title('FlexiGraph')
+root.geometry('500x500')
+root.configure(bg=col_black)
+icon = tk.PhotoImage(file='icon.png')
+root.iconphoto(True, icon)
 
+#Create the main frames
+searchframe = tk.Frame(root, bg=col_darkblue)
+searchframe_elements = tk.Frame(searchframe, bg = col_darkblue)
+returnframe = tk.Frame(root, bg=col_darkblue)
+graphframe = tk.Frame(root, bg=col_black)
+#Frame Layout
+searchframe.grid(column=0, row=0, sticky = 'EW')
+searchframe_elements.grid(column=1, row=0)
+searchframe.grid_columnconfigure(0, weight=1)
+searchframe.grid_columnconfigure(1, weight=0)
+searchframe.grid_columnconfigure(2, weight=1)
+graphframe.grid(column=0, row=2)
+root.grid_rowconfigure(2,weight=1)
+root.grid_columnconfigure(0, weight=1)
+
+#Create search frame elements (search box, etc.) and variable to store results,
+search_label = tk.Label(searchframe_elements, text='Stock Ticker:', font=default_font, fg=col_white, bg=col_darkblue)
+stock_name = tk.StringVar()
+search_box = tk.Entry(searchframe_elements, textvariable=stock_name, font=default_font)
+search_button = tk.Button(master = searchframe_elements, command = tickerHandling, height=1, width=8, text='Search', bd=2, bg=col_grey, fg=col_white, activebackground=col_white, activeforeground=col_black)
+search_label.grid(row=0,column=0, pady=10, padx=(10,5))
+search_box.grid(row=0,column=1, pady=10)
+search_button.grid(row=0,column=2,pady=10, padx=18)
+
+#Create return frame elements
+return_label = tk.Label(returnframe, text = '', font=error_font, fg=col_red, bg=col_darkblue)
+return_label.grid(sticky='EW')
+
+#Create graph frame elements
+canvas = tk.Canvas(graphframe, width=0, height=0)
+
+#Initialise GUI loop
+search_box.focus()
+root.mainloop()
